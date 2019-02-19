fa13095c1ab84fd7d9743c463f9f30bfcfc7b4a5
Ali-X <alina240895@yandex.ru>
2019-02-19 13:36:57 +0200

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 window.task1 = function task1() {
-  let xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
 
   xhr.open('GET', 'https://mate-academy.github.io/phone-catalogue-static/phones/phones.json', true);
 
   xhr.send();
 
   xhr.onreadystatechange = function() { // (3)
-    if (xhr.readyState != 4) return;
+    if (xhr.readyState !== 4) return;
 
-    if (xhr.status != 200) {
+    if (xhr.status !== 200) {
       alert(xhr.status + ': ' + xhr.statusText);
     } else {
       console.log(xhr.responseText);
@@ -17,46 +17,46 @@ window.task1 = function task1() {
 };
 
 window.task2 = function task2() {
-  let xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
 
   xhr.open('GET', 'https://tanuhaua.github.io/datas-file-json/data.json', true);
 
   xhr.send();
 
   xhr.onreadystatechange = function() { // (3)
-    if (xhr.readyState != 4) return;
+    if (xhr.readyState !== 4) return;
 
-    if (xhr.status != 200) {
+    if (xhr.status !== 200) {
       alert(xhr.status + ': ' + xhr.statusText);
     } else {
-      let ANCESTRY_FILE = JSON.parse(xhr.responseText);
+      const ANCESTRY_FILE = JSON.parse(xhr.responseText);
 
       createCards();
 
       function createCard(parentElem, human) {
-        let divElem = document.createElement("div");
+        const divElem = document.createElement("div");
         divElem.setAttribute("class", "card");
 
-        let nameElem = document.createElement("div");
+        const nameElem = document.createElement("div");
         nameElem.innerText = human.name;
         nameElem.setAttribute("class", "card-item card-name");
-        let sexElem = document.createElement("div");
+        const sexElem = document.createElement("div");
         sexElem.setAttribute("class", "card-item card-sex");
 
-        if (human.sex === "m") {
+        if (human['sex'] === "m") {
           sexElem.innerText = "man";
         } else {
           sexElem.innerText = "woman";
         }
 
-        let agesElem = document.createElement("div");
-        agesElem.innerText = human.born + " - " + human.died;
+        const agesElem = document.createElement("div");
+        agesElem.innerText = human['born'] + " - " + human['died'];
         agesElem.setAttribute("class", "card-item card-age");
-        let fatherElem = document.createElement("div");
-        fatherElem.innerText = "Father: " + human.father;
+        const fatherElem = document.createElement("div");
+        fatherElem.innerText = "Father: " + human['father'];
         fatherElem.setAttribute("class", "card-item card-father");
-        let motherElem = document.createElement("div");
-        motherElem.innerText = "Mother: " + human.mother;
+        const motherElem = document.createElement("div");
+        motherElem.innerText = "Mother: " + human['mother'];
         motherElem.setAttribute("class", "card-item card-mother");
 
         divElem.appendChild(nameElem);
@@ -69,7 +69,7 @@ window.task2 = function task2() {
       }
 
       function createCards() {
-        let divElem = document.createElement("div");
+        const divElem = document.createElement("div");
         divElem.setAttribute("class", "cards");
 
         ANCESTRY_FILE.forEach(elem => createCard(divElem, elem));
@@ -82,17 +82,17 @@ window.task2 = function task2() {
 
 window.task3 = function task3() {
   let visitorsTable;
-  let xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
 
   xhr.open('GET', 'https://tanuhaua.github.io/datas-file-json/visitors.json', true);
 
   xhr.send();
 
   xhr.onreadystatechange = function() { // (3)
-    if (xhr.readyState != 4) return;
+    if (xhr.readyState !== 4) return;
 
     function addClickListeners() {
-      let headers = document.getElementsByClassName('table__header');
+      const headers = document.getElementsByClassName('table__header');
 
       for (let i = 0; i < headers.length; i++) {
         headers[i].addEventListener('click', () => {
@@ -127,16 +127,16 @@ window.task3 = function task3() {
       }
     }
 
-    if (xhr.status != 200) {
+    if (xhr.status !== 200) {
       alert(xhr.status + ': ' + xhr.statusText);
     } else {
       visitorsTable = JSON.parse(xhr.responseText);
       sortByKeyAsc('id');
 
-      let tableElem = document.createElement('table');
+      const tableElem = document.createElement('table');
       tableElem.classList.add('table');
 
-      let rowElem = createHeaderRow();
+      const rowElem = createHeaderRow();
 
       tableElem.appendChild(rowElem);
       fillTable(visitorsTable, tableElem);
@@ -178,9 +178,9 @@ window.task4 = function task4() {
   let counter = 1;
   let visitorsObj;
 
-  let tableElem = document.getElementsByClassName('table')[0];
+  const tableElem = document.getElementsByClassName('table')[0];
 
-  let rowElem = createHeaderRow();
+  const rowElem = createHeaderRow();
   tableElem.appendChild(rowElem);
 
   sendRequest();
@@ -188,13 +188,13 @@ window.task4 = function task4() {
   addClickListeners();
 
   function sendRequest() {
-    let link = `https://tanuhaua.github.io/datas-file-json/dynamic-loading/${counter}/users.json`;
-    let xhr = new XMLHttpRequest();
+    const link = `https://tanuhaua.github.io/datas-file-json/dynamic-loading/${counter}/users.json`;
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', link, true);
     xhr.send();
     xhr.onreadystatechange = function() { // (3)
-      if (xhr.readyState != 4) return;
-      if (xhr.status != 200) {
+      if (xhr.readyState !== 4) return;
+      if (xhr.status !== 200) {
         alert(xhr.status + ': ' + xhr.statusText);
       } else {
         visitorsObj = JSON.parse(xhr.responseText);
@@ -219,16 +219,16 @@ window.task4 = function task4() {
 };
 
 function createHeaderRow() {
-  let rowElem = document.createElement('tr');
+  const rowElem = document.createElement('tr');
   rowElem.classList.add('table__headers');
 
-  let headerIdElem = createElement('th', 'table__header', 'Visitor id', 'id');
+  const headerIdElem = createElement('th', 'table__header', 'Visitor id', 'id');
   headerIdElem.classList.add('js-sort-asc');
 
-  let headerRegElem = createElement('th', 'table__header', 'Registration date', 'createdAt');
-  let headerNameElem = createElement('th', 'table__header', 'Name', 'name');
-  let headerEmailElem = createElement('th', 'table__header', 'Email', 'email');
-  let headerDescrElem = createElement('th', 'table__header', 'Description', 'description');
+  const headerRegElem = createElement('th', 'table__header', 'Registration date', 'createdAt');
+  const headerNameElem = createElement('th', 'table__header', 'Name', 'name');
+  const headerEmailElem = createElement('th', 'table__header', 'Email', 'email');
+  const headerDescrElem = createElement('th', 'table__header', 'Description', 'description');
 
   rowElem.appendChild(headerIdElem);
   rowElem.appendChild(headerRegElem);
@@ -241,16 +241,16 @@ function createHeaderRow() {
 
 function fillTable(arr, tableElem) {
   arr.forEach((elem) => {
-    let date = new Date(elem['createdAt']);
-    let rowElem = document.createElement('tr');
+    const date = new Date(elem['createdAt']);
+    const rowElem = document.createElement('tr');
     rowElem.classList.add('table__row');
 
-    let idElem = createElement('td', 'table__data', elem['id']);
-    let regElem = createElement('td', 'table__data',
+    const idElem = createElement('td', 'table__data', elem['id']);
+    const regElem = createElement('td', 'table__data',
       date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2));
-    let nameElem = createElement('td', 'table__data', elem['name']);
-    let emailElem = createElement('td', 'table__data', elem['email']);
-    let descrElem = createElement('td', 'table__data', elem['description']);
+    const nameElem = createElement('td', 'table__data', elem['name']);
+    const emailElem = createElement('td', 'table__data', elem['email']);
+    const descrElem = createElement('td', 'table__data', elem['description']);
 
     rowElem.appendChild(idElem);
     rowElem.appendChild(regElem);
@@ -263,7 +263,7 @@ function fillTable(arr, tableElem) {
 }
 
 function createElement(tagName, htmlClass, innerText, id) {
-  let elem = document.createElement(tagName);
+  const elem = document.createElement(tagName);
   elem.classList.add(htmlClass);
   elem.innerText = innerText;
   elem.id = id;
